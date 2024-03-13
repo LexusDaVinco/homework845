e83f54b84fd45b77f3a60b37702d6b2feb574786
AlexiusDeVinco <119763746+LexusDaVinco@users.noreply.github.com>
2024-03-13 23:39:14 +0100

    fixxx

--- save_json.js
+++ save_json.js
@@ -7,7 +7,7 @@ const save = async (favNumber) => {
   console.log("saving");
   await s3.putObject({
     Body: JSON.stringify(favNumber, null, 2),
-    Bucket: "cyclic-busy-jade-dibbler-wig-us-east-1gu",
+    Bucket: "cyclic-busy-jade-dibbler-wig-us-east-1",
     Key: "number.json",
   }).promise()
 };
